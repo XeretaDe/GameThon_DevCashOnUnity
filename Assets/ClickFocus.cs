171a118a0ff2089a2ac7a09d73baf9712fc3c4e8
XeretaDe <rodrigomcariello@gmail.com>
2021-10-16 17:34:01 -0300

    Add Arquivos Base
    Adição dos arquivos Unity + Alguns assets principais

---
+++ Assets/ClickFocus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ClickFocus : MonoBehaviour
+{
+    [SerializeField] protected TextMeshProUGUI Score;
+    int Value = 0;
+
+    void Start()
+    {
+
+    }
+
+    public void OnClick() 
+    {
+        Value += 1;
+
+        Score.text = "Grana: " + Value;
+    }
+
+
+
+}
